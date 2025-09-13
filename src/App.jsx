df3b253d13441f2acdc21bcb56e1a524877cac80
AylaFatullayeva <aylasgachaworld@gmail.com>
2025-09-13 14:49:17 +0400

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+       <h1 className='bg-red-500'>Vite + React</h1>
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
